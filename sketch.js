f2195e1da9a2239c2cb8fe051db9dc60d77c3c64
YouZiLing <150638580+YouZiLing@users.noreply.github.com>
2024-06-03 11:27:57 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -104,11 +104,12 @@ function drawSkeleton() {
     }
           // 頭上學號
     if (pose.keypoints[0].score > 0.1) {
+      translate(video.width, 0);
       scale(-1,1)
       textAlign(CENTER);
       textSize(50);
       fill(255);
-      text("412730185", pose.keypoints[0].x, pose.keypoints[0].y -90);
+      text("412730185", pose.keypoints[0].x, pose.keypoints[0].y -100);
     }
   }
 }
